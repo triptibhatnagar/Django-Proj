13ed9ab49ac0a7b79e2f33c49cfe78dbdeb6ccdb
triptibhatnagar <bhatnagartripti2727@gmail.com>
2025-08-17 23:08:12 +0530

    add nav

---
+++ PracDjango/proj1/proj1/views.py
@@ -0,0 +1,18 @@
+# user --> req --> urls.py --> views.py --> res --> user
+
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("Hello Django, this is homepage")
+    # return render(request, 'index.html')
+    return render(request, 'website/index.html')
+    # templates to tumhe pta hee h, bs vhi se ye file utha lo
+
+def about(request):
+    # return HttpResponse("Hello Django, this is about page")
+    return render(request, 'website/about.html')
+
+def contact(request):
+    # return HttpResponse("Hello Django, this is contact page")
+    return render(request, 'website/contact.html')
