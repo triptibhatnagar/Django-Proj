2689290f983df85d5937513658085186dfae1cc5
triptibhatnagar <bhatnagartripti2727@gmail.com>
2025-08-20 14:18:03 +0530

    added proj 1

---
+++ Py-Django/proj1/proj1/views.py
@@ -0,0 +1,15 @@
+from django.http import request, HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("this is my home page")
+    return render(request, 'index.html')
+
+def about(request):
+    return HttpResponse("this is my about page")
+
+def contact(request):
+    return HttpResponse("this is my contact page")
+
+def help(request):
+    return HttpResponse("this is my help page")
