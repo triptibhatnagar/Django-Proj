3e71be69250280c6f514180086db41fdc0e95561
triptibhatnagar <bhatnagartripti2727@gmail.com>
2025-08-27 14:34:57 +0530

    proj3 done

--- Py-Django/proj3/enroll/admin.py
+++ Py-Django/proj3/enroll/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from enroll.models import Student
 
 # Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'created_at')
+    search_fields = ('name', 'email')
+    list_filter = ('age',)
+    ordering = ('-created_at',)
+
+admin.site.register(Student, StudentAdmin)
--- Py-Django/proj3/enroll/models.py
+++ Py-Django/proj3/enroll/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # how record shows in django admin
+    def __str__(self):
+        return self.name
